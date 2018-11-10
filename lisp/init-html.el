31864464ee7ac95df7cfc89e47f0e12830a676d1
Prashanth Iyer <munichlinux@gmail.com>
2018-11-09 17:40:19 -0800

    Local modifications.

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,12 +1,16 @@
 (require-package 'tagedit)
+(require-package 'emmet-mode)
+
 (after-load 'sgml-mode
   (tagedit-add-paredit-like-keybindings)
   (define-key tagedit-mode-map (kbd "M-?") nil)
   (define-key tagedit-mode-map (kbd "M-s") nil)
   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
-(add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
+(add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\|hbs\\)\\'")
 
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'sgml-mode-hook 'emmet-mode)
 ;; Note: ERB is configured in init-ruby
 
 (provide 'init-html)
--- lisp/init-mmm.el
+++ lisp/init-mmm.el
@@ -2,9 +2,10 @@
 ;; Multiple major modes
 ;;----------------------------------------------------------------------------
 (require-package 'mmm-mode)
+(require-package 'neotree)
 (require 'mmm-auto)
 (setq mmm-global-mode 'buffers-with-submode-classes)
 (setq mmm-submode-decoration-level 2)
-
+(global-set-key [f8] 'neotree-toggle)
 
 (provide 'init-mmm)
